a89b0038ccfb0614cb25c98d799349dfa28758f0
ringolip <zhangringo@gmail.com>
2018-12-02 16:46:31 +0800

    微信开发者接口V2.0 增加自动回复功能

--- wechat.py
+++ wechat.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 from flask import Flask, request, abort
 import hashlib 
+import xmltodict
+import time
 
 RINGO_TOKEN = "ringo" # 微信接口Token
 
@@ -15,13 +17,12 @@ app = Flask(__name__)
 # 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
 
 
-@app.route('/ringochat') # 根据微信借口url设置路由地址
+@app.route('/ringochat', methods=["GET", "POST"]) # 根据微信借口url设置路由地址
 def ringochat():
     data = request.args # 微信的查询字符串
     signature = data.get("signature")
     nonce  = data.get("nonce")
     timestamp = data.get("timestamp")
-    echostr = data.get("echostr")
 
     if not all([signature, timestamp, nonce]):
         abort(400)
@@ -32,11 +33,45 @@ def ringochat():
     data_str = "".join(li) # 拼接字符串
     hash_obj = hashlib.sha1(data_str) # 生成加密对象
     hash_str = hash_obj.hexdigest() # 转化为加密字符串
-    
+
     if signature != hash_str: # 检验signature对请求进行校验
         abort(403)
     else:
-        return echostr # 原样返回echostr参数内容
+        if request.method == "GET": #若请求方式为GET即为请求验证
+            echostr = request.args.get("echostr") # 只有在验证时才会传入echostr
+            return echostr # 原样返回echostr参数内容
+        elif request.method == "POST":
+            if request.data:
+                xml_str = request.data
+                xml_dict = xmltodict.parse(xml_str) # 将xml字符串转换为字典类型
+                xml_dict = xml_dict.get("xml") # 继续提取xml键中的内容
+
+                if xml_dict.get("MsgType") == "text": # 如果发送的内容为文本格式
+                    # 构造回复的xml
+                    return_dict = {
+                        "xml":{
+                            "ToUserName": xml_dict.get("FromUserName"),
+                            "FromUserName": xml_dict.get("ToUserName"),
+                            "CreateTime": int(time.time()),
+                            "MsgType": "text",
+                            "Content": xml_dict.get("Content"),
+                        }  
+                    }
+                    return_xml_str = xmltodict.unparse(return_dict)
+                    return return_xml_str # 返回回复的xml字符串
+                else: # 如果是其他格式
+                    return_dict = {
+                        "xml":{
+                            "ToUserName": xml_dict.get("FromUserName"),
+                            "FromUserName": xml_dict.get("ToUserName"),
+                            "CreateTime": int(time.time()),
+                            "MsgType": "text",
+                            "Content": u"现在只支持文字回复啦QAQ",
+                        }  
+                    }
+                    return_xml_str = xmltodict.unparse(return_dict)
+                    return return_xml_str # 返回回复的xml字符串
+
 
 
 if __name__ == "__main__":
